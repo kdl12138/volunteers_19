a624a6ba35786b90565feec93dae8cce0aca4494
lwj <1345539017@qq.com>
2019-11-27 15:26:26 +0800

    [update]作业

--- 吕王靖/Eratosthenes法筛选素数.cpp
+++ 吕王靖/Eratosthenes法筛选素数.cpp
@@ -28,27 +28,3 @@ int main(){
 	return 0;
 }
 
-/*int main(){
-	int i,j;
-	int a[10005];
-	int n = 0;
-	for(i = 1;i<10001;i++){
-		a[i] = 1;
-	}
-	for(i = 2;i<10001;i++){
-		for(j = 2;i*j<10001;j++){
-			a[i*j] = 0;
-		}
-		while(i<10001 && a[i] == 0)
-		i++;
-	}
-	for(i = 2;i<10001;i++){
-		if(a[i]!=0){
-			printf("%5d",i);
-			n++;
-			if(n%10 == 0)
-			printf("\n");
-		}
-	}
-	return 0;
-}*/
