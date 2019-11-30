003f49077bb12daa6b3813ddb88611ef049afa8e
krokami <2806353279@qq.com>
2019-11-30 22:39:49 +0800

    [update]修改扫雷

--- 李玉苗/9.cpp
+++ 李玉苗/9.cpp
@@ -1,7 +1,8 @@
+/*不循环还是好康些（尬笑.jpg 
 #include <stdio.h>
 const int maxn = 50;
 int main(){
-	int m, n, count = 0; 
+	int m, n; 
 	int mine[maxn][maxn];
 	
 	scanf("%d %d", &m, &n);
@@ -15,7 +16,7 @@ int main(){
 		}
 	}
 	for(int i = 1; i < m + 1; i++){
-		for(int j = 1; j < n + 2; j++){
+		for(int j = 2; j < n + 2; j++){
 			if(mine[i][j] != 0){
 				if(mine[i - 1][j - 1] == 0)
 					mine[i][j]++;
@@ -33,19 +34,66 @@ int main(){
 					mine[i][j]++;
 				if(mine[i + 1][j + 1] == 0)
 					mine[i][j]++;
-			}	
-		}
-	}
-	for(int i = 1; i < m + 1; i++)
-		for(int j = 2; j < n + 2; j++){
-			if(mine[i][j] != 0)
 				printf("%d ", mine[i][j] - 1);
+			}
 			else
-				printf("* ");
-			count++;
-			if(count % n == 0)
-				printf("\n");		
+				printf("* ");	
 		}
+		printf("\n");
+	}
 			
 	return 0;
-}
+}*/
+
+
+#include <stdio.h>
+const int maxn = 50;
+int main(){
+	char mine[maxn][maxn];
+	int m, n, count;
+	
+	scanf("%d %d", &m, &n);
+	for(int i = 0; i < m + 2; i++)
+		for(int j = 0; j < n + 2; j++)
+			mine[i][j] = '?';
+			
+	for(int i = 1; i < m + 1; i++)
+		for(int j = 1; j < n + 2; j++)
+			mine[i][j] = getchar();
+			
+	for(int i = 1; i < m + 1; i++){
+		for(int j = 1; j < n + 2; j++){
+			if(mine[i][j] == '*')
+				printf("* ");
+			if(mine[i][j] == '?'){
+				count = 0;
+				for(int p = i - 1; p < i + 2; p++)
+					for(int q = j - 1; q < j + 2; q++)
+						if(mine[p][q] == '*')
+							count++;
+				printf("%d ", count);
+			}
+		}
+		printf("\n");
+	}
+	
+	return 0;
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
