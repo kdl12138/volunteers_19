c238362d4f57f2afed8af5ead63effee5d8dcc4b
lwj <1345539017@qq.com>
2019-12-07 10:49:20 +0800

    [update]更新扫雷，使用循环（我觉得八个if真的挺好）

--- 吕王靖/扫雷小游戏.cpp
+++ 吕王靖/扫雷小游戏.cpp
@@ -36,3 +36,33 @@ int main(){
 	}
 return 0;
 }
+
+#include<stdio.h>
+int main(){
+	char a[52][52]; 
+	int i,j;
+	int n,m;
+	int x,y;
+	scanf("%d %d\n",&n,&m);
+	for(i = 1;i<=n;i++){
+		for(j = 1;j<=m+1;j++)
+		a[i][j] = getchar();
+	//	scanf("%c",&a[i][j]);
+	}
+	for(i = 1;i<=n;i++){
+		for(j = 1;j<=m;j++){
+			if(a[i][j]!='*'){
+				a[i][j] = '0';	
+				for(x = i-1;x<=i+1;x++){
+					for(y = j-1;y<=j+1;y++){
+						if(a[x][y] == '*')
+						a[i][j]++;
+					}
+				}
+			}
+			printf("%c",a[i][j]);
+		}
+		printf("\n");
+	}
+return 0;
+}
